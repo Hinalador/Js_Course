83c33e9c59df5b954e4332c6204ce44e814e77d1
Hinalador <jacevedosalazar@gmail.com>
2024-12-03 10:19:04 -0500

    Creacion de variables e inicio de datatypes

---
+++ 00-helloworld.js
@@ -0,0 +1,20 @@
+//comentarios
+ 
+/*comentario
+en
+varias
+lineas */
+
+//console.log
+console.log("Hello world!")
+console.log('Hello world!')
+console.log(`Hello world!`)
+
+console.log("5")
+console.log(5)
+console.log(5 + 5)
+console.log(5 - 5)
+console.log(5 * 5)
+console.log(5 / 5)
+console.log(5 % 5)
+console.log(5 ** 5)
---
+++ 01-variables.js
@@ -0,0 +1,17 @@
+// var
+
+var helloWorld = 'Hola mundo!'
+console.log(helloWorld)
+
+helloWorld = 'Hola mundo!'
+console.log(helloWorld)
+
+//let
+
+let helloWorld1 = 'Hola mundo! 1'
+console.log(helloWorld1)
+
+//const
+
+const helloWorld2 = 'Hola mundo! 2'
+console.log(helloWorld2)
