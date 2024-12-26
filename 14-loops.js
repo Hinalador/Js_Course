36c211c3780bc18312e23e451603f68de8dc579f
Hinalador <jacevedosalazar@gmail.com>
2024-12-25 21:59:26 -0500

    creacion de loops

---
+++ 14-loops.js
@@ -0,0 +1,33 @@
+//Loops o bucles
+
+// for
+
+for (let i = 0; i < 5 ; i++) {
+    console.log(`Hola ${i}`)
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+for (let i = 0; i < numbers.length ; i++) {
+    console.log(`Elemento ${numbers[i]}`)
+}
+
+// While
+
+let i = 0
+while (i < 5) {
+    console.log(`Hola ${i}`)
+    i++
+}
+
+// do while
+
+i = 6
+do {
+    console.log(`Hola ${i}`)
+    i++
+} while(i < 5)
+
+for () {
+    console.log("for of")
+}
